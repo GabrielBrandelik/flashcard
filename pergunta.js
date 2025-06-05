45a6da00f9edc6e1c0d2dad166c3757094613100
Gabriel Brandelik <g.choma@escola.pr.gov.br>
2025-06-05 10:56:09 -0300

    Add files via upload

--- pergunta.js
+++ pergunta.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Matemática',
-    'qual o valor de pi?',
-    '3,14'
+    'Qual valor de pi?',
+    'Aproximadamente 3,14'
 )
 
 criaCartao(
@@ -14,4 +14,10 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
