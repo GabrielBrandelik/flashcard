b30d8f25695e23072359270828b5656219c26908
Gabriel Brandelik <g.choma@escola.pr.gov.br>
2025-06-05 12:42:56 -0300

    Add files via upload

--- pergunta.js
+++ pergunta.js
@@ -21,3 +21,51 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'História',
+    'Quando ocorreu a revolução na França?',
+    '1789'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual órgão do corpo bombeia sangue?',
+    'Coração'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'Qual o primeiro grupo de plantas?',
+    'Briófitas'
+)
+
+criaCartao(
+    'Robótica',
+    'Oque é um Led?',
+    'É um emissor de luz'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quem foi Marx?',
+    'Foi um sociólogo importante para a história'
+)
+
+criaCartao(
+    'Projeto de vida',
+    'oque você pensa em fazer no futuro?',
+    'Estar cursando medicina'
+)
+
+criaCartao(
+    'Resolução de problemas',
+    'oque é índice de cocção?',
+    'É a alteração da massa do alimento depois de cozido'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O cartão de crédito endivida?',
+    'Se não usado corretamente sim'
+)
